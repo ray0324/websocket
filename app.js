a677ae15d3feac0e4a8935e652ad2dc9ff1dd9a0
ray0324 <ray0324@foxmail.com>
2018-06-11 00:31:27 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const Websocket = require('./lib/websocket');
+const net = require('net');
+const http =  require('http');
+
+// const clientList = new clientList;
+const PORT = 4000;
+
+const server = net.createServer();
+// const httpServer = http.createServer();
+
+const ws = new Websocket;
+
+server.on('connection',socket => {
+
+  
+  ws.once('message', function (msg) {
+    res.end(msg);
+  });
+  ws.setSocket(socket);
+  // httpServer.on('connection', function (req, res) {
+  //   ws.once('message',function(msg){
+  //     res.end(msg);
+  //   });
+  //   ws.send(req);
+  // });
+});
+
+// httpServer.listen(5000, function () {
+//   console.log('http server start at 5000');
+// })
+
+server.listen(PORT, function () {
+  console.log('websocket server run port ' + PORT);
+});
---
+++ doc/websocket.md
@@ -0,0 +1,15 @@
+# Websocket 详解
+
+[协议RFC文档](https://tools.ietf.org/html/rfc6455#section-5.1)
+
+## 
+
+## 握手
+
+## 建立连接
+
+## 帧格式类型
+
+- ping
+- pong
+- 
